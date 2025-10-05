429530b18611b44476020c9958fea49af8b22cde
fikremariam <fikremariamdebela8@gmail.com>
2025-10-05 21:19:50 +0300

    testing for test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,43 +5,43 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
     
-    def test_add_positive_numbers(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(5, 3), 8)
     
-    def test_add_negative_numbers(self):
+    def test_addition_negative_numbers(self):
         self.assertEqual(self.calc.add(-5, -3), -8)
     
-    def test_add_mixed_numbers(self):
+    def test_addition_mixed_numbers(self):
         self.assertEqual(self.calc.add(5, -3), 2)
     
-    def test_subtract_positive_numbers(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 4), 6)
     
-    def test_subtract_negative_numbers(self):
+    def test_subtraction_negative_numbers(self):
         self.assertEqual(self.calc.subtract(-5, -3), -2)
     
-    def test_subtract_mixed_numbers(self):
+    def test_subtraction_mixed_numbers(self):
         self.assertEqual(self.calc.subtract(5, -3), 8)
     
-    def test_multiply_positive_numbers(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(6, 7), 42)
     
-    def test_multiply_negative_numbers(self):
+    def test_multiplication_negative_numbers(self):
         self.assertEqual(self.calc.multiply(-6, 7), -42)
     
-    def test_multiply_by_zero(self):
+    def test_multiplication_by_zero(self):
         self.assertEqual(self.calc.multiply(6, 0), 0)
     
-    def test_divide_positive_numbers(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
     
-    def test_divide_negative_numbers(self):
+    def test_division_negative_numbers(self):
         self.assertEqual(self.calc.divide(-10, 2), -5)
     
-    def test_divide_fraction_result(self):
+    def test_division_fraction_result(self):
         self.assertEqual(self.calc.divide(5, 2), 2.5)
     
-    def test_divide_by_zero(self):
+    def test_division_by_zero(self):
         with self.assertRaises(ValueError):
             self.calc.divide(10, 0)
 
