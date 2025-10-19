ad343cf86a4db3caa340cad3b9024580451f3f6d
fikremariam <fikremariamdebela8@gmail.com>
2025-10-19 21:49:31 +0300

    task 2 fix

--- task_2.sql
+++ task_2.sql
@@ -1,23 +1,23 @@
 -- USE the database
 USE alx_book_store;
 
--- CREATE TABLE: AUTHORS
+-- CREATE TABLE: Authors
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- CREATE TABLE: BOOKS
+-- CREATE TABLE: Books
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- CREATE TABLE: CUSTOMERS
+-- CREATE TABLE: Customers
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -25,21 +25,21 @@ CREATE TABLE IF NOT EXISTS Customers (
     address TEXT
 );
 
--- CREATE TABLE: ORDERS
+-- CREATE TABLE: Orders
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- CREATE TABLE: ORDER_DETAILS
+-- CREATE TABLE: Order_Details
 CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
